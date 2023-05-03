a9ad999e3971c03918c714ebee39f6906c8f47b0
Antonio arieiro <sfgfffs@gmail.com>
2023-05-03 16:55:31 -0300

    fix: print translation

--- main.py
+++ main.py
@@ -8,38 +8,44 @@ from sklearn.preprocessing import LabelEncoder
 
 # lendo o dataset
 df = pd.read_excel('dataset.xlsx')
-### CORRELAÇÕES
-def correlations_display():
-      # exibindo correlações
-      correlations = df.corrwith(df['SARS-Cov-2 exam result'])
-      print('columns with greater correlations')
-      print(correlations.sort_values(ascending=False).head(10))
-      print('\n\n')
-      print('columns with smaller correlations')
-      print(correlations.sort_values(ascending=True).head(10))
-      print('\n\n')
-      print(correlations.abs().sort_values(ascending=False).head(10))
-      print('\n\n')
-      
-### RESULTADOS
-# calculando acurácia
-def display_results(model):
-      prds = model.predict(X_test)
-      tn, fp, fn, tp = confusion_matrix(y_test, prds).ravel()
-      print(f'tn {tn}, fp {fp}, fn {fn}, tp {tp}', '\n\n',
-            'Accuracy:', (accuracy_score(y_test, prds)), '\n\n',
-            'Classification Report:\n', (classification_report(y_test, prds)))
+# CORRELAÇÕES
 
-### PRE-PROCESSAMENTO
+
+def correlations_display():
+    # exibindo correlações
+    correlations = df.corrwith(df['SARS-Cov-2 exam result'])
+    print('colunas com maiores correlações')
+    print(correlations.sort_values(ascending=False).head(10))
+    print('\n\n')
+    print('colunas com correlações menores')
+    print(correlations.sort_values(ascending=True).head(10))
+    print('\n\n')
+    print(correlations.abs().sort_values(ascending=False).head(10))
+    print('\n\n')
+
+# RESULTADOS
+# calculando acurácia
+
+
+def display_results(model):
+    prds = model.predict(X_test)
+    tn, fp, fn, tp = confusion_matrix(y_test, prds).ravel()
+    print(f'tn {tn}, fp {fp}, fn {fn}, tp {tp}', '\n\n',
+          'Accuracy:', (accuracy_score(y_test, prds)), '\n\n',
+          'Classification Report:\n', (classification_report(y_test, prds)))
+
+
+# PRE-PROCESSAMENTO
 # removendo colunas irrelevantes
-df = df.drop(columns=['Patient ID', 'Patient addmited to regular ward (1=yes, 0=no)', 'Patient addmited to semi-intensive unit (1=yes, 0=no)', 'Patient addmited to intensive care unit (1=yes, 0=no)'])
+df = df.drop(columns=['Patient ID', 'Patient addmited to regular ward (1=yes, 0=no)',
+             'Patient addmited to semi-intensive unit (1=yes, 0=no)', 'Patient addmited to intensive care unit (1=yes, 0=no)'])
 
 # mapeando dados categorizados
 for column in df.columns:
-      if df[column].dtype == 'object': 
-            lbl = LabelEncoder()
-            lbl.fit(list(df[column].values))
-            df[column] = lbl.transform(list(df[column].values))
+    if df[column].dtype == 'object':
+        lbl = LabelEncoder()
+        lbl.fit(list(df[column].values))
+        df[column] = lbl.transform(list(df[column].values))
 
 # checando a coluna de resultados
 print(df['SARS-Cov-2 exam result'].value_counts(normalize=True))
@@ -48,14 +54,15 @@ print(df['SARS-Cov-2 exam result'].value_counts(normalize=True))
 df = df[df.columns[df.isna().sum()/df.shape[0] < 0.9]]
 df = df.fillna(df.median())
 
-### TREINAMENTO
+# TREINAMENTO
 # criando listas X (features) e y (resultado esperado)
 # X = df[['Platelets', 'Leukocytes', 'Monocytes', 'Patient age quantile']]
 X = df.select_dtypes(include='number')
 y = df['SARS-Cov-2 exam result']
 
 # separando 66% do dataset para treinamento e 33% para validação
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state=101)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=101)
 
 
 # criando o modelo e treinando
